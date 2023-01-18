6342f1a9703a11ebb36f3099a8d36b2da55a7166
ThomasEvers <ThomasEvers>
2023-01-18 18:02:13 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    celsius = request.args.get("celsius", "")
+    if celsius:
+        fahrenheit = fahrenheit_from(celsius)
+    else:
+        fahrenheit = ""
+    return (
+        #"Helloooo"
+        render_template("index.html")
+        )
+
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
